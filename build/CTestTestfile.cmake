5f5b871cc429e405ca7e04a7f11d0466b14ea75a
Vidhant Sharma <vidhantsharma@gmail.com>
2024-11-12 23:05:36 +0530

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vidhant/hitech_robot_assignment/hitech_robot_ws/src
+# Build directory: /home/vidhant/hitech_robot_assignment/hitech_robot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("hitech_robot_description")
---
+++ build/hitech_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vidhant/hitech_robot_assignment/hitech_robot_ws/src/hitech_robot_description
+# Build directory: /home/vidhant/hitech_robot_assignment/hitech_robot_ws/build/hitech_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
