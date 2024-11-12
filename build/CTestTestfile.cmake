a1c6bd6cfe79669f100531bdc84380e335ced2c3
Vidhant Sharma <vidhantsharma@gmail.com>
2024-11-13 01:08:44 +0530

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vidhant/hitech_robot_assignment/hitech_robot_ws/src
-# Build directory: /home/vidhant/hitech_robot_assignment/hitech_robot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("hitech_robot_description")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/vidhant/hitech_robot_assignment/hitech_robot_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/vidhant/hitech_robot_assignment/hitech_robot_ws/devel;/home/vidhant/bumperbot_project/my_work/bumperbot_ws/devel;/opt/ros/noetic'.split(';') if '/home/vidhant/hitech_robot_assignment/hitech_robot_ws/devel;/home/vidhant/bumperbot_project/my_work/bumperbot_ws/devel;/opt/ros/noetic' != '' else []
